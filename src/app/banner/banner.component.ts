43b8b3c23e93f0a0edf28c7035fe137509fb3e4b
Roman Rogowski <rogowski@zeropointdynamics.com>
2018-11-25 16:57:38 -0500

    toggle icon

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  @Output() onDrawerToggle = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleDrawer() {
+    this.onDrawerToggle.emit();
   }
 
 }
